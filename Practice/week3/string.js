dae4a681292eb10b907354dbdd87ecd2780adcbd
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-06-04 22:37:37 +0900

    [FEAT] 3주차 실습 내용
    js의 자료형 별 특징

---
+++ Practice/week3/string.js
@@ -0,0 +1,10 @@
+var lang = 'JavaScript';
+var love = '사랑하'
+var you = null;
+var num = 5;
+console.log(lang + null + love + num);
+
+var msg1 = 'She said "Good bye"';
+var msg2 = "She's gone.";
+console.log(msg1);
+console.log(msg2);
---
+++ Practice/week3/variable.js
@@ -0,0 +1,11 @@
+var msg = 'Hello';
+
+msg = 'World';
+console.log(msg);
+
+msg = 100;
+console.log(msg);
+
+var count = 10;
+msg = count;
+console.log(msg);
